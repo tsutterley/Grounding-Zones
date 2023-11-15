73debe20d44a7e1a5c711e0119558a3ee657571d
tsutterley <tsutterl@uw.edu>
2023-11-15 15:47:39 -0800

    Update version.py

--- grounding_zones/version.py
+++ grounding_zones/version.py
@@ -10,6 +10,6 @@ metadata = importlib.metadata.metadata("grounding_zones")
 # get version
 version = metadata["version"]
 # append "v" before the version
-full_version = "v{0}".format(version)
+full_version = f"v{version}"
 # get project name
 project_name = metadata["Name"]
